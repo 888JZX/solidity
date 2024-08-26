46c01019cb125f671da6c00a2f1b04b20af6df6c
888JZX <3525549618@qq.com>
2024-08-26 19:18:34 +0800

    SimpleStorage

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+contract SimpleStorage{
+   uint256 number;
+   struct People{
+    string name;
+    uint256 number;
+   } 
+   People[] public people;
+   mapping(string=>uint256) public nametonum;
+   function store(uint256 n) public{
+     number=n;
+   }
+   function retrieve() public view returns(uint256){
+    return number;
+   }
+   function addPerson(string memory _name,uint256 _number) public{
+    people.push(People(_name,_number));
+    nametonum[_name]=_number;
+
+   }
+
+
+
+
+}
